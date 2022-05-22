e408df73ab7c785fb15ff04939a3347edd88decc
ik-top-digital <top.digital.ik@gmail.com>
2022-05-22 12:40:55 +0300

    Getting statrted with Flutter and Firebase SDK

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
-const ChatScreen({ Key? key }) : super(key: key);
+  const ChatScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: (ctx, i) => Container(padding:const EdgeInsets.all(8.0), child: const Text('This works!'),),
+        itemBuilder: (ctx, i) => Container(
+          padding: const EdgeInsets.all(8.0),
+          child: const Text('This works!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/CJgWjxHJROC0uUDWgcOz/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
       ),
     );
   }
