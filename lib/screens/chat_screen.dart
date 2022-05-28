553b5bd56ffc70172facffb826685503493b655a
ik-top-digital <top.digital.ik@gmail.com>
2022-05-28 17:15:47 +0300

    Listening to Chat Messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_chat_firebase/widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -40,26 +41,12 @@ class ChatScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: StreamBuilder<dynamic>(
-          stream: FirebaseFirestore.instance
-              .collection('chats/CJgWjxHJROC0uUDWgcOz/messages')
-              .snapshots(),
-          builder: (ctx, streamSnapshot) {
-            if (streamSnapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final documents = streamSnapshot.data.docs;
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (ctx, i) => Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(documents[i]['text']),
-              ),
-            );
-          },
-        ),
+        body: Container(child: Column(children: [
+        Expanded(
+          child: Messages(),
+          ),
+        
+        ],),),
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.add),
             onPressed: () {
---
+++ lib/widgets/chat/messages.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (BuildContext ctx, AsyncSnapshot<QuerySnapshot> chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        
+        if (chatSnapshot.hasError) {
+          return Text('Error loading data: ${chatSnapshot.error}');
+        }
+        
+        final chatDocs = chatSnapshot.data!.docs;
+        
+                return ListView.builder(
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) => Text(chatDocs[index]['text']),
+        );
+      },
+    );
+  }
+}
